37c4eba43f7a8c1584e34a55e2a33485b66fb00d
Hossam <hossamsherraa@gmail.com>
2023-01-02 12:59:49 +0200

    Quality

--- Tests/CompressImageTests/CompressImageTests.swift
+++ Tests/CompressImageTests/CompressImageTests.swift
@@ -6,7 +6,7 @@ final class CompressImageTests: XCTestCase {
     func testExample() throws {
         let urlPath = Bundle.module.url(forResource: "image", withExtension: "jpg")!.path
         let real = UIImage(contentsOfFile: urlPath)!
-        let result = real.wxCompress()
+        let result = real.wxCompress(compressionQuality: 0.3)
         
         print(real.pngData() , result.pngData())
        
